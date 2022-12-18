b9d9ac10eae9a341f9c2eaabb9bf7ce1674d5999
KyawZinThant22 <kyawthantver2@gmail.com>
2022-12-18 15:44:23 +0630

    first commit

---
+++ src/pages/dashboard/GeneralAnalyticsPage.tsx
@@ -0,0 +1,155 @@
+import { Helmet } from 'react-helmet-async';
+// @mui
+import { useTheme } from '@mui/material/styles';
+import { Grid, Container, Typography } from '@mui/material';
+// _mock_
+import { _analyticPost, _analyticOrderTimeline, _analyticTraffic } from '../../_mock/arrays';
+// components
+import { useSettingsContext } from '../../components/settings';
+// sections
+import {
+  AnalyticsTasks,
+  AnalyticsNewsUpdate,
+  AnalyticsOrderTimeline,
+  AnalyticsCurrentVisits,
+  AnalyticsWebsiteVisits,
+  AnalyticsTrafficBySite,
+  AnalyticsWidgetSummary,
+  AnalyticsCurrentSubject,
+  AnalyticsConversionRates,
+} from '../../sections/@dashboard/general/analytics';
+
+// ----------------------------------------------------------------------
+
+export default function GeneralAnalyticsPage() {
+  const theme = useTheme();
+
+  const { themeStretch } = useSettingsContext();
+
+  return (
+    <>
+      <Helmet>
+        <title> General: Analytics | Minimal UI</title>
+      </Helmet>
+
+      <Container maxWidth={themeStretch ? false : 'xl'}>
+        <Typography variant="h4" sx={{ mb: 5 }}>
+          Hi, Welcome back
+        </Typography>
+
+        <Grid container spacing={3}>
+          <Grid item xs={12} sm={6} md={3}>
+            <AnalyticsWidgetSummary
+              title="Weekly Sales"
+              total={714000}
+              icon={'ant-design:android-filled'}
+            />
+          </Grid>
+
+          <Grid item xs={12} sm={6} md={3}>
+            <AnalyticsWidgetSummary
+              title="New Users"
+              total={1352831}
+              color="info"
+              icon={'ant-design:apple-filled'}
+            />
+          </Grid>
+
+          <Grid item xs={12} sm={6} md={3}>
+            <AnalyticsWidgetSummary
+              title="Item Orders"
+              total={1723315}
+              color="warning"
+              icon={'ant-design:windows-filled'}
+            />
+          </Grid>
+
+          <Grid item xs={12} sm={6} md={3}>
+            <AnalyticsWidgetSummary
+              title="Bug Reports"
+              total={234}
+              color="error"
+              icon={'ant-design:bug-filled'}
+            />
+          </Grid>
+
+          <Grid item xs={12}>
+            <AnalyticsWebsiteVisits
+              title="Website Visits"
+              subheader="(+43%) than last year"
+              chart={{
+                labels: [
+                  '01/01/2003',
+                  '02/01/2003',
+                  '03/01/2003',
+                  '04/01/2003',
+                  '05/01/2003',
+                  '06/01/2003',
+                  '07/01/2003',
+                  '08/01/2003',
+                  '09/01/2003',
+                  '10/01/2003',
+                  '11/01/2003',
+                ],
+                series: [
+                  {
+                    name: 'Team A',
+                    type: 'area',
+                    fill: 'gradient',
+                    data: [23, 11, 22, 27, 13, 22, 37, 21, 44, 22, 30],
+                  },
+                ],
+              }}
+            />
+          </Grid>
+
+          <Grid item xs={12}>
+            <AnalyticsConversionRates
+              title="Conversion Rates"
+              subheader="(+43%) than last year"
+              chart={{
+                series: [
+                  { label: 'Italy', value: 400 },
+                  { label: 'Japan', value: 430 },
+                  { label: 'China', value: 448 },
+                  { label: 'Canada', value: 470 },
+                  { label: 'France', value: 540 },
+                  { label: 'Germany', value: 580 },
+                  { label: 'South Korea', value: 690 },
+                  { label: 'Netherlands', value: 1100 },
+                  { label: 'United States', value: 1200 },
+                  { label: 'United Kingdom', value: 1380 },
+                ],
+              }}
+            />
+          </Grid>
+
+          <Grid item xs={12} md={6} lg={8}>
+            <AnalyticsNewsUpdate title="News Update" list={_analyticPost} />
+          </Grid>
+
+          <Grid item xs={12} md={6} lg={4}>
+            <AnalyticsOrderTimeline title="Order Timeline" list={_analyticOrderTimeline} />
+          </Grid>
+
+          <Grid item xs={12} md={6} lg={4}>
+            <AnalyticsTrafficBySite title="Traffic by Site" list={_analyticTraffic} />
+          </Grid>
+
+          <Grid item xs={12} md={6} lg={8}>
+            <AnalyticsTasks
+              title="Tasks"
+              list={[
+                { id: '1', label: 'Create FireStone Logo' },
+                { id: '2', label: 'Add SCSS and JS files if required' },
+                { id: '3', label: 'Stakeholder Meeting' },
+                { id: '4', label: 'Scoping & Estimations' },
+                { id: '5', label: 'Sprint Showcase' },
+              ]}
+            />
+          </Grid>
+        </Grid>
+      </Container>
+    </>
+  );
+}
